761b644453b51867532e4b21427c5f42d0b13ed3
Felix Ouma <moxiemeyoh@gmail.com>
2023-08-10 11:33:52 +0300

    dynamic category implementation

--- src/App.js
+++ src/App.js
@@ -3,9 +3,26 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+// obtaining unique category list from the items
+const allCategories = ['all',...new Set(items.map(item => item.category))];
+
+//console.log(allCategories);
+
 function App() {
   const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState(allCategories);
+
+  //to filter items according to their cartegories
+  //after filtering, the filtered items, based on a category will be passed to the setMenuItems
+  const filterItems = (category) => {
+    if (category === 'all') {
+        setMenuItems(items);
+        return;
+    };
+
+    const newItems = items.filter( item => item.category === category);
+    setMenuItems(newItems);
+  }
 
   return(
   <main>
@@ -14,7 +31,7 @@ function App() {
         <h2>our menu</h2>
         <div className='underline'></div>
       </div>
-      <Categories/>
+      <Categories filterItems={filterItems} categories= {categories}/>
       <Menu items={menuItems}/>
     </section>
   </main>
--- src/Menu.js
+++ src/Menu.js
@@ -5,10 +5,17 @@ const Menu = ({items}) => {
   <div className='section-center'>
     {
     items.map((menuItem) => {
-      const {id, img, price, title, decs} = menuItem;
+      const {id, img, price, title, desc} = menuItem;
       return (
         <article key={id} className='menu-item'>
           <img src={img} alt={title} className='photo'/>
+          <div className='info'>
+            <header>
+              <h4>{title}</h4>
+              <h4 className='price'>${price}</h4>
+            </header>
+            <p className='item-text'>{desc}</p>
+          </div>
         </article>
       )
     })
